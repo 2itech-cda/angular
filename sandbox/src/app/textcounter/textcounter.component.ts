c6d82b04a844111d440f3057a42cf0e1d92faaa4
Vincent Letourneau <vincent@nanoninja.com>
2020-11-17 11:15:02 +0100

    Add Input properties

--- sandbox/src/app/textcounter/textcounter.component.ts
+++ sandbox/src/app/textcounter/textcounter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-textcounter',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./textcounter.component.scss']
 })
 export class TextcounterComponent implements OnInit {
+  @Input() content: string = '';
+  @Input('max') maxLength: number = 10;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
