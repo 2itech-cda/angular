b4ba96fc6ce0754fb5645424c43d14620cbcaf74
Vincent Letourneau <vincent@nanoninja.com>
2020-11-20 16:50:56 +0100

    Add login form

--- myapp/src/app/components/user/login/login.component.ts
+++ myapp/src/app/components/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/services/notification.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private notification: NotificationService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]]
+    });
   }
 
+  onSubmit(): void {
+    if (this.form.valid) {
+      this.userService.register(this.form.value)
+          .subscribe();
+    }
+  }
+
+  hasError(name: string, validator: string): boolean {
+    return this.form.controls[name].hasError(validator);
+  }
 }
